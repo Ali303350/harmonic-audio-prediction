ddc7c5888cdff87c6ecb8dab23ff94d2091fce86
Faresal1 <faresalatawna71@gmail.com>
2025-06-23 15:09:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ if __name__ == "__main__":
 
     print("### Comparing recordings ###")
     compare_recordings(path1, path2)
+
+    print("\n### LPC on Rec1 ###")
+    rate1, data1 = load_wav(path1)
+    seg1 = select_segment(data1, rate1)
+    a1, pred1, err1 = lpc_analysis(seg1, order=16)
+    print("LPC Coefficients Rec1:", a1)
+    plot_lpc(seg1, pred1, rate1, "Rec1: LPC Prediction")
+    plot_lpc_coefficients(a1, "Rec1: LPC Coefficients")
+    plot_lpc_error_vs_order(seg1, rate1, title="Rec1: LPC Prediction Error vs Order")
