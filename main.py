23244b16d475b26d030fc092f6006168f379e667
Ali Ali <amaq262@gmail.com>
2025-06-23 15:17:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from src.analysis.compare_recordings import compare_recordings
-
+from src.visualization.plot_waveform import plot_waveform
+from src.analysis.compute_periodogram import compute_periodogram
+from src.visualization.plot_periodogram import plot_periodogram
+from src.analysis.estimate_f0_autocorr import estimate_f0_autocorr
+from src.analysis.fit_harmonics import fit_harmonics
+from src.visualization.plot_original_vs_harmonic_fit import plot_original_vs_harmonic_fit
+from src.visualization.plot_harmonic_energy_vs_f0 import plot_harmonic_energy_vs_f0
+from src.visualization.plot_harmonic_coefficients import plot_harmonic_coefficients
 
 
 
